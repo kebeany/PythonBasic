b67cbcc322d030b733fef49fbaea42e9c5948826
kebeany <138075261+kebeany@users.noreply.github.com>
2023-06-28 19:59:10 -0400

    Create main.py

---
+++ 01/main.py
@@ -0,0 +1,32 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+# print("Hello world")
+# print(1)
+# print(123)
+# print(10.22)
+# print(-10.22)
+# a = 3
+# b = 4
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# a = 3
+# b = 4
+# print (a ** b) # 3*3*3*3=81
+# a = 7
+# b = 4 
+# print (a / b) # 7/4= 1.75
+# print(a // b) # 7//4= 1
+# print(a % b) # 7%4= 3
+# print(b % a) # 4%7= 4
+# print (-a) # -7
+# print(-b) # -3
+# a = -7
+# b = -3
+# print(-a) # --7=7
+# print(-b) # --3=3
+print(1 + 2 * 3) 
+print((1 + 2) *3 )
+print((2 * 3) / 4 + 1)
+print(2 * 3 / (4 + 1))
