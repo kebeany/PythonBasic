1af5473040f39b1b2105f6bf6e1ac9870bcecfbd
kebeany <138075261+kebeany@users.noreply.github.com>
2023-06-29 20:04:25 -0400

    Create main.py

---
+++ 02/main.py
@@ -0,0 +1,39 @@
+# # print('hello world!')
+# # print((1000*3)+(500*2)+(700*1)+(1500*4)) 
+# # red_pen_price = 1000
+# # pencil_price =  500
+# # eraser_price = 700
+# # black_pen_price = 1500
+# # total_price = red_pen_price * 3 + pencil_price * 2 + eraser_price *1 + black_pen_price * 4
+# # # print(total_price)
+# # print( 'hi py ')
+# # print("hi py")
+# # print('''hi py''')
+# # print("""hi py""")
+# # print("hello py.\n this is a random person")
+# # print('''hello py.
+# # this is a random person''')
+# # print("""hello py.
+# # # this is a random person""")
+# # head = 'python'
+# # tails = ' is fun'
+# # print( head + tails )
+# # string = ' python'
+# # # print(string *3 )
+# # hello= 'hello!'
+# # print(hello[5])
+# # print(hello[-1])
+# # print(hello[1])
+# # print(hello[-5])
+# S = "hello I am studying python"
+# print(S[0:5])
+# print(S[5:7])
+# print (S[7:9])
+# print(S[19:])
+# print(S[:18])
+s= "I ate {0} apples"
+print(s.format(1000))
+n=1000
+print(s.format(n))
+print(s.format('5'))
+print(s.format('five'))
