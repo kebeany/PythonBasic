d2d56388979400d3df7a58b7408bda8d89511c14
kebeany <138075261+kebeany@users.noreply.github.com>
2023-07-03 20:02:20 -0400

    Create main.py

---
+++ 04/main.py
@@ -0,0 +1,50 @@
+x=3
+y=2
+print(x>y)#true
+print(x<y)#false
+print(x==y)#false
+print(x!=y)#true
+print(x>=y)#true
+print(x<=y)#false
+print('Python'=='Python')#true
+print('Python'=='python')#false
+print('whale coding'!='whale coding')#false
+print(3*2==6)#true
+print('*'*2=='**')#true
+print(10==10 and 10 !=5) #true
+print((10>5 or 10<3))#true
+print(not 10>=5)#false
+print(not 10<10)#true
+apple = int(input('how many apples do you have?'))
+if apple==3:
+    print( 'ate apple')
+else:
+    print('didn\'t eat apple')
+money = 4000
+if money>=10000:
+    print("you have alot of money")
+else:
+    print("You have not much money ")
+
+money = int(input('how much money do you have?'))
+if money>=10000:
+    print("you have alot of money")
+else:
+    print("You do not have much money ")
+print("1")
+if False:
+    print("2")
+else:
+    if False:
+        print("3")
+    else:
+        print("4")
+print("5")
+# 1,4,5
+
+number = 5
+string1 = 'False'
+string2 = "5*2"
+string3 = '''9-5'''
+bool1 = True
+bool2 = 1==1
