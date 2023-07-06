8d9943e395daaebedf8b4c18538add42b896c5fa
kebeany <138075261+kebeany@users.noreply.github.com>
2023-07-05 20:01:17 -0400

    Create main.py

---
+++ 06/main.py
@@ -0,0 +1,53 @@
+hit = 0
+while hit < 100:
+    hit = hit +1
+    print("tree was hit{}".format(hit))
+hit = 0 
+while hit<10: 
+    hit= hit +1
+    print("the tree was hit {}".format(hit))
+if hit == 10:
+    print("tree was hit 10 times")
+hit = 0 
+while hit<10: 
+    hit= hit +1
+    print("the tree was hit {}".format(hit))
+    if hit == 10:
+        print("tree was hit 10 times")
+    elif hit == 8:
+        print("tree was hit 8 times.")
+    elif hit ==5:
+        print("tree was hit 5 times")
+number = 0
+while number != 100:
+    print("Enter 100 ")
+    number= int(input())
+number = 0
+while True:
+    number = number +1
+    print(number)
+for i in range(1,13,1):
+    print("{}".format(i))
+i:0,1,2
+3 is range
+for i in range (3):
+    print("t")
+for i in range(2,6,1):
+    print(i)
+
+for i in range(5):
+    print(i)
+
+for i in range(10,0,-1):
+    print(i)
+for number in range(4):
+    if number == 2:
+        continue
+    print(number)
+for number in range(4):
+    if number == 2:
+        break
+    print(number)
+number = int(input('enter number'))
+for i in range(number):
+    print('I love python')
